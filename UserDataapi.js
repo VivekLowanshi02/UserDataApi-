429f2454216f7e90bcc46267855cd041c56b31d7
Vivek Lowanshi <134102884+VivekLowanshi02@users.noreply.github.com>
2023-08-25 19:16:32 +0530

    Add files via upload

---
+++ UserDataapi.js
@@ -0,0 +1,68 @@
+[
+    {
+        "userId":1,
+        "id": "1",
+        "name": "Vivek lowanshi",
+        "email":"Thane@technical.com",
+        "websites": "https://www.youtube.com/viveklowanshi",
+        "mobile": "12345678",
+        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQTGbjDdY2-sEZN9mMIZORGdm4zk9RFycbPT0WITLOU1HNIWK3yMtPNvp7P49UBKPXpvkU-ORlm1Wb71T4",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur rem dignissimos, inventore blanditiis sapiente dolorum ad dolore dicta nostrum libero reiciendis sit minima iure architecto quidem tempore quisquam amet"
+    },
+
+    {
+        "userId":2,
+        "id": "2",
+        "name": "Rupam lovvanshi",
+        "email":"Thane@technical.com",
+        "websites": "https://www.youtube.com/viveklowanshi",
+        "mobile": "12345678",
+        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQTGbjDdY2-sEZN9mMIZORGdm4zk9RFycbPT0WITLOU1HNIWK3yMtPNvp7P49UBKPXpvkU-ORlm1Wb71T4",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur rem dignissimos, inventore blanditiis sapiente dolorum ad dolore dicta nostrum libero reiciendis sit minima iure architecto quidem tempore quisquam amet"
+    },
+
+    {
+        "userId":3,
+        "id": "3",
+        "name": "Dhirendra ",
+        "email":"Thane@technical.com",
+        "websites": "https://www.youtube.com/viveklowanshi",
+        "mobile": "12345678",
+        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQTGbjDdY2-sEZN9mMIZORGdm4zk9RFycbPT0WITLOU1HNIWK3yMtPNvp7P49UBKPXpvkU-ORlm1Wb71T4",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur rem dignissimos, inventore blanditiis sapiente dolorum ad dolore dicta nostrum libero reiciendis sit minima iure architecto quidem tempore quisquam amet"
+    },
+
+    {
+        "userId":4,
+        "id": "4",
+        "name": "chandan Designer",
+        "email":"Thane@technical.com",
+        "websites": "https://www.youtube.com/viveklowanshi",
+        "mobile": "12345678",
+        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQTGbjDdY2-sEZN9mMIZORGdm4zk9RFycbPT0WITLOU1HNIWK3yMtPNvp7P49UBKPXpvkU-ORlm1Wb71T4",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur rem dignissimos, inventore blanditiis sapiente dolorum ad dolore dicta nostrum libero reiciendis sit minima iure architecto quidem tempore quisquam amet"
+    },
+
+    {
+        "userId":5,
+        "id": "5",
+        "name": "Abhi nanadan",
+        "email":"Thane@technical.com",
+        "websites": "https://www.youtube.com/viveklowanshi",
+        "mobile": "12345678",
+        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQTGbjDdY2-sEZN9mMIZORGdm4zk9RFycbPT0WITLOU1HNIWK3yMtPNvp7P49UBKPXpvkU-ORlm1Wb71T4",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur rem dignissimos, inventore blanditiis sapiente dolorum ad dolore dicta nostrum libero reiciendis sit minima iure architecto quidem tempore quisquam amet"
+    },
+
+    {
+        "userId":6,
+        "id": "6",
+        "name": "ankit rajput",
+        "email":"Thane@technical.com",
+        "websites": "https://www.youtube.com/viveklowanshi",
+        "mobile": "12345678",
+        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQTGbjDdY2-sEZN9mMIZORGdm4zk9RFycbPT0WITLOU1HNIWK3yMtPNvp7P49UBKPXpvkU-ORlm1Wb71T4",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur rem dignissimos, inventore blanditiis sapiente dolorum ad dolore dicta nostrum libero reiciendis sit minima iure architecto quidem tempore quisquam amet"
+    },
+
+];
